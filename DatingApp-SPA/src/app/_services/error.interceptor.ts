78ded79855d39836cbd5ade91bc470c7a08fac91
webdbmike12345 <webdbmike@gmail.com>
2020-04-27 11:47:43 -0500

    End of section 9

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpInterceptor,
-  HttpErrorResponse,
-  HTTP_INTERCEPTORS,
-} from '@angular/common/http';
+import { HttpInterceptor, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
@@ -14,25 +10,25 @@ export class ErrorInterceptor implements HttpInterceptor {
     next: import('@angular/common/http').HttpHandler
   ): import('rxjs').Observable<import('@angular/common/http').HttpEvent<any>> {
     return next.handle(req).pipe(
-      catchError((error) => {
+      catchError(error => {
         if (error.status === 401) {
           return throwError(error.statusText);
         }
-        if (error instanceof HttpErrorResponse) {
+        if (error instanceof HttpErrorResponse){
           const applicationError = error.headers.get('Application-Error');
-          if (applicationError) {
+          if(applicationError) {
             return throwError(applicationError);
           }
           const serverError = error.error;
-          let modalStateErrors = '';
-          if (serverError.errors && typeof serverError.errors === 'object') {
-            for (const key in serverError.errors) {
-              if (serverError.errors[key]) {
-                modalStateErrors += serverError.errors[key] + '\n';
+          let modelStattErrors = '';
+          if(serverError && typeof serverError === 'object') {
+            for(const key in serverError) {
+              if(serverError[key]) {
+                modelStattErrors += serverError[key] + '\n';
               }
             }
           }
-          return throwError(modalStateErrors || serverError || 'Server Error');
+          return throwError(modelStattErrors || serverError || 'Server Error');
         }
       })
     );
@@ -41,6 +37,6 @@ export class ErrorInterceptor implements HttpInterceptor {
 
 export const ErrorInterceptorProvider = {
   provide: HTTP_INTERCEPTORS,
-  useClass: ErrorInterceptor,
-  multi: true,
+  userClass: ErrorInterceptor,
+  multi: true
 };
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,20 +1,104 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { RouterModule } from '@angular/router';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { HttpClientModule } from '@angular/common/http';
+// import { FormsModule } from '@angular/forms';
+// import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+// import { RouterModule } from '@angular/router';
+// import { JwtModule } from '@auth0/angular-jwt';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+// import { AppComponent } from './app.component';
+// import { NavComponent } from './nav/nav.component';
+// import { AuthService } from './_services/auth.service';
+// import { HomeComponent } from './home/home.component';
+// import { RegisterComponent } from './register/register.component';
+// import { ErrorInterceptorProvider,ErrorInterceptor } from './_services/error.interceptor';
+// import { MemberListComponent } from './members/member-list/member-list.component';
+// import { ListsComponent } from './lists/lists.component';
+// import { MessagesComponent } from './messages/messages.component';
+// import { appRoutes } from './routes';
+// import { MemberCardComponent } from './members/member-card/member-card.component';
+
+// export function tokenGetter(){
+//    return localStorage.getItem('token');
+// }
+
+// // 'localhost:5000/api/auth'
+// @NgModule({
+//    declarations: [
+//       AppComponent,
+//       NavComponent,
+//       HomeComponent,
+//       RegisterComponent,
+//       MemberListComponent,
+//       ListsComponent,
+//       MessagesComponent,
+//       MemberCardComponent
+//    ],
+//    imports: [
+//       BrowserModule,
+//       HttpClientModule,
+//       FormsModule,
+//       BrowserAnimationsModule,
+//       BsDropdownModule.forRoot(),
+//       RouterModule.forRoot(appRoutes),
+//       JwtModule.forRoot({
+//          config: {
+//             tokenGetter,
+//             whitelistedDomains: ['localhost:5000'],
+//             blacklistedRoutes: []
+//          }
+//       })
+//    ],
+//    providers: [
+//       AuthService,
+//       ErrorInterceptor
+//    ],
+//    bootstrap: [
+//       AppComponent
+//    ]
+// })
+// export class AppModule { }
+
 import { AuthService } from './_services/auth.service';
+import { BrowserModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxGalleryModule } from 'ngx-gallery';
+ 
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { MemberListComponent } from './member-list/member-list.component';
+import { ErrorInterceptorProvider, ErrorInterceptor } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+ 
+ 
+export function tokenGetter() {
+   return  localStorage.getItem('token');
+ }
+ 
+export class CustomHammerConfig extends HammerGestureConfig  {
+   overrides = {
+       pinch: { enable: false },
+       rotate: { enable: false }
+   };
+}
 
 @NgModule({
    declarations: [
@@ -24,18 +108,34 @@ import { appRoutes } from './routes';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      TabsModule.forRoot(),
+      BrowserAnimationsModule,
+      NgxGalleryModule,
+      RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+         config: {
+            tokenGetter: tokenGetter,
+            whitelistedDomains: ['localhost:5000'],
+            blacklistedRoutes: ['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
-      AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptor,
+      AlertifyService,
+      AuthGuard,
+      MemberDetailResolver,
+      MemberListResolver,
+      { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
    bootstrap: [
       AppComponent
